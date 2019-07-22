1f7b19e79dfd9fb97e0d34e8c67b677a1ecf5473
huanliang19 <32529135+huanliang19@users.noreply.github.com>
2019-07-21 18:42:30 -0700

    Add files via upload
    Initial Formatting

--- Block.py
+++ Block.py
@@ -1,50 +1,79 @@
 from PIL import Image, ImageDraw, ImageFont
 
-def wrap_text(text, width, font):
-    text_lines = []
-    text_line = []
-    text = text.replace('\n', ' [br] ')
-    words = text.split()
-    font_size = font.getsize(text)
-
-    for word in words:
-        if word == '[br]':
-            text_lines.append(' '.join(text_line))
-            text_line = []
-            continue
-        text_line.append(word)
-        w, h = font.getsize(' '.join(text_line))
-        if w > width:
-            text_line.pop()
-            text_lines.append(' '.join(text_line))
-            text_line = [word]
-
-    if len(text_line) > 0:
-        text_lines.append(' '.join(text_line))
-
-    return text_lines
-
 class block:
-    def __init__(self, xy, width, height):
-        x, y = xy
-        self.width = 0
-        self.height = 0
+    # canvas = Image.new('1',(0, 0),255)
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.canvas = Image.new('1',(width, height),255)
 
-    def printText(self, text):
-        pass
+    # font = ImageFont.load_default()
+    # font_size = 1
+    # draw = ImageDraw.Draw(canvas)
+    # text = ""
+
+    def setFont(self, font):
+        self.font = font
+
+    def setFontSize(self, font_size):
+        self.font_size = font_size
+
+    def update(self):
+        print('helloz')
+        return Image.open('100x100.bmp')
 
 class timeBlock(block):
-    def setTime(hour, minutes, ampm):
-        pass
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+    def update(self):
+        print('helloz')
+        return Image.open('100x100.bmp')
 
 class dateBlock(block):
-    def setDate(month, day, year):
-        pass
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+    def update(self):
+        return Image.open('100x100.bmp')
 
 class dayBlock(block):
-    def setDay(day):
-        pass
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+    def update(self):
+        return Image.open('100x100.bmp')
 
 class factBlock(block):
-    def updateFact():
-        pass
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+    def update(self):
+        text = "filler text"
+        # super().text = wrap_text(text, width=400, font=font)
+        # super().text = "\n".join(text)
+        return Image.open('100x100.bmp')
+
+    def wrap_text(text, width, font):
+        text_lines = []
+        text_line = []
+        text = text.replace('\n', ' [br] ')
+        words = text.split()
+        font_size = font.getsize(text)
+
+        for word in words:
+            if word == '[br]':
+                text_lines.append(' '.join(text_line))
+                text_line = []
+                continue
+            text_line.append(word)
+            w, h = font.getsize(' '.join(text_line))
+            if w > width:
+                text_line.pop()
+                text_lines.append(' '.join(text_line))
+                text_line = [word]
+
+        if len(text_line) > 0:
+            text_lines.append(' '.join(text_line))
+
+        return text_lines
--- main.py
+++ main.py
@@ -1,56 +1,49 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
-import epd4in2
+# import epd4in2
+import Block as blk
 import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
 
+timeLoc = (0,0)
+dateLoc = (200,0)
+dayLoc = (200,40)
+factLoc = (0,80)
+
 try:
-    epd = epd4in2.EPD()
-    epd.init()
-    epd.Clear(0xFF)
+#     epd = epd4in2.EPD()
+#     epd.init()
+#     epd.Clear(0xFF)
 
     # Drawing on the Horizontal image
-    Himage = Image.new('1', (epd4in2.EPD_WIDTH, epd4in2.EPD_HEIGHT), 255)  # 255: clear the frame
-    # Drawing on the Vertical image
-    Limage = Image.new('1', (epd4in2.EPD_HEIGHT, epd4in2.EPD_WIDTH), 255)  # 255: clear the frame
-
-    # Horizontal
-    print("Drawing")
+    # Himage = Image.new('1', (epd4in2.EPD_WIDTH, epd4in2.EPD_HEIGHT), 255)  # 255: clear the frame
+    Himage = Image.new('1', (400, 300), 255)  # 255: clear the frame
     draw = ImageDraw.Draw(Himage)
-    font24 = ImageFont.truetype('/usr/share/fonts/truetype/wqy/wqy-microhei.ttc', 24)
-    draw.text((10, 0), 'hello world', font = font24, fill = 0)
-    draw.text((10, 20), '2.9inch e-Paper', font = font24, fill = 0)
-    draw.text((150, 0), u'微雪电子', font = font24, fill = 0)
-    draw.line((20, 50, 70, 100), fill = 0)
-    draw.line((70, 50, 20, 100), fill = 0)
-    draw.rectangle((20, 50, 70, 100), outline = 0)
-    draw.line((165, 50, 165, 100), fill = 0)
-    draw.line((140, 75, 190, 75), fill = 0)
-    draw.arc((140, 50, 190, 100), 0, 360, fill = 0)
-    draw.rectangle((80, 50, 130, 100), fill = 0)
-    draw.chord((200, 50, 250, 100), 0, 360, fill = 0)
-    epd.display(epd.getbuffer(Himage))
+    # font24 = ImageFont.truetype('/usr/share/fonts/truetype/wqy/wqy-microhei.ttc', 24)
+
+    timeBlk = blk.timeBlock(200,80)
+    dateBlk = blk.dateBlock(200,40)
+    dayBlk = blk.dayBlock(200,40)
+    factBlk = blk.factBlock(400,220)
+
+    timeImg = timeBlk.update()
+    dayImg = dateBlk.update()
+    dateImg = dayBlk.update()
+    factImg = factBlk.update()
+
+    bmp = Image.open('100x100.bmp')
+    Himage.paste(timeImg, timeLoc)
+    Himage.paste(bmp, dateLoc)
+    Himage.paste(bmp, dayLoc)
+    Himage.paste(bmp, factLoc)
+
+    # epd.display(epd.getbuffer(Himage))
+    Himage.show()
     time.sleep(2)
 
-    # # Vertical
-    # draw = ImageDraw.Draw(Limage)
-    # font18 = ImageFont.truetype('/usr/share/fonts/truetype/wqy/wqy-microhei.ttc', 18)
-    # draw.text((2, 0), 'hello world', font = font18, fill = 0)
-    # draw.text((2, 20), '2.9inch epd', font = font18, fill = 0)
-    # draw.text((20, 50), u'微雪电子', font = font18, fill = 0)
-    # draw.line((10, 90, 60, 140), fill = 0)
-    # draw.line((60, 90, 10, 140), fill = 0)
-    # draw.rectangle((10, 90, 60, 140), outline = 0)
-    # draw.line((95, 90, 95, 140), fill = 0)
-    # draw.line((70, 115, 120, 115), fill = 0)
-    # draw.arc((70, 90, 120, 140), 0, 360, fill = 0)
-    # draw.rectangle((10, 150, 60, 200), fill = 0)
-    # draw.chord((70, 150, 120, 200), 0, 360, fill = 0)
-    # epd.display(epd.getbuffer(Limage))
-    # time.sleep(2)
-    #
+
     # print("read bmp file")
     # Himage = Image.open('4in2.bmp')
     # epd.display(epd.getbuffer(Himage))
@@ -63,7 +56,7 @@ try:
     # epd.display(epd.getbuffer(Himage2))
     # time.sleep(2)
 
-    epd.sleep()
+    # epd.sleep()
 
 except:
     print('traceback.format_exc():\n%s',traceback.format_exc())
